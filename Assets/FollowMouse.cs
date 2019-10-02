19118f6677c00036aba3555e207f44e660b0b34c
Matt <kudell95@hotmail.com>
2019-10-02 18:47:09 +1000

    changed movement to rigidbody

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    Vector3 lookPos;
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            
+            lookPos =   hit.point - this.transform.position;
+            lookPos.y = 0;
+            
+           
+        }
+        if(Vector3.Distance(hit.point, transform.position) > 1)
+        {
+            Quaternion lookrot = Quaternion.LookRotation(lookPos);
+            transform.transform.rotation = Quaternion.Slerp(transform.rotation, lookrot, Time.deltaTime * speed);
+        }
+    }
+}
--- Assets/TopDownController.cs
+++ Assets/TopDownController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +7,26 @@ public class TopDownController : MonoBehaviour
 
     public float moveSpeed;
     Vector3 movement;
+    Rigidbody rb;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
 
         movement = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
         
-        Debug.Log(movement.magnitude);
-        transform.Translate(movement*Time.deltaTime*moveSpeed, Space.World);
+        // Debug.Log(movement.magnitude);
+        // transform.Translate(movement*Time.deltaTime*moveSpeed, Space.World);
+
+        rb.MovePosition(transform.position + (movement * (Time.deltaTime * moveSpeed)));
+
         
     }
 }
