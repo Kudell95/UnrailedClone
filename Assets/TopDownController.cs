1e75ac63804e039e7a53b26acdc6a8084e9056db
Matt <kudell95@hotmail.com>
2019-10-02 16:53:58 +1000

    initial commit

---
+++ Assets/TopDownController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    Vector3 movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        movement = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        
+        Debug.Log(movement.magnitude);
+        transform.Translate(movement*Time.deltaTime*moveSpeed, Space.World);
+        
+    }
+}
