f4c9c96016ce113ce49378d1ec790228ac6aacf8
Matt <kudell95@hotmail.com>
2019-10-03 10:29:38 +1000

    started item pickup

--- Assets/player.cs
+++ Assets/player.cs
@@ -8,8 +8,33 @@ public class player : MonoBehaviour
     List<Item> itemsInHand = new List<Item>();
     // Start is called before the first frame update
     
+    public Transform itemHolder;
+    public float OverlapSpheresize = 1f;
+
+    void Upadte(){
+        Collider[] hitcolliders;
+        hitcolliders = Physics.OverlapSphere(itemHolder.position, OverlapSpheresize);
+
+        if(hitcolliders.Length > 0)
+        {
+            StartCoroutine(FindItemsToPickup(hitcolliders));
+        }
+        
+    }
 
 
+    IEnumerator FindItemsToPickup(Collider[] hits)
+    {   
+        
+        int i=0;
+        while (i < hits.Length)
+        {
+            hits[i].SendMessage("Item Found - " + hits[i].gameObject.name);
+            i++;
+        }
 
+
+        yield return null;
+    }
 
 }
